d9cc91d0e9c8755e8c0941cc64e9d14153948dda
Brian Moore <brian.moore@cloudruler.io>
2021-04-07 02:35:36 +0000

    Updated init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,3 +7,14 @@ username=$AZ_DO_USERNAME
 password=$AZ_DO_PAT
 
 EOF
+
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/aks-engine
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/connectivity
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/devops
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/identity
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/management
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/initialization
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/management
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/sandbox
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/scripts
+git clone https://cloudruler@dev.azure.com/cloudruler/infrastructure/_git/terraform-cloudruler-common
